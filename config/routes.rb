32c1de73be1226da765654299597fe128c74f3ee
Julien Maisonnier <julien_maisonnier@yahoo.com>
2018-08-13 20:01:00 +0200

    01-Stupid Coaching, update with proper link_to syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'ask', to: 'questions#ask', as: :ask
-  get 'answer', to: 'questions#answer', as: :answer
+  get 'answer', to: 'pages#answer', as: :answer
   # get ask_path, to: 'questions#ask', as: :ask
 end
